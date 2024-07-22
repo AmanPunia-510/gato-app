e46756559db663d7ce9235b16d74b96b8a8fe844
AmanPunia-510 <puniaaman888@gmail.com>
2024-07-22 17:16:52 +0530

    compress all webp

--- src/components/Verification.jsx
+++ src/components/Verification.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Email from '../assets/images/webp/Email.webp'
 import Ellips from '../assets/images/webp/Our_mission_right.webp'
 import HandEllips from '../assets/images/webp/Footer-ellips.webp'
 
 const Verification = () => {
+    const [formValue, setFormValue] = useState({
+        name: "",
+        email: ""
+    })
+    const [error, setError] = useState(false)
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if (formValue.name !== "" && formValue.email !== "") {
+            setError(false)
+        } else {
+            setError(true)
+        }
+        console.log(formValue, "formValue")
+    }
+    console.log(error, "error")
     return (
         <section className='pt-20 sm:pt-24 md:pt-[126px] lg:pt-[150px] xl:pt-[227px] relative max-w-[1531px] mx-auto'>
             <div className="container relative z-10 xxl:max-w-[1440px]">
@@ -12,14 +27,22 @@ const Verification = () => {
                     <div className="w-full lg:w-5/12 max-lg:flex max-lg:justify-center">
                         <form action="" className='max-w-[471px] w-full'>
                             <div className='px-8 py-6 md:py-7 border-solid border-[3px] border-primary rounded-[72px] bg-white'>
-                                <input type="text" name="Enter your name" className='font-inter font-normal text-md leading-130 text-primary border-none outline-none w-full' placeholder='Enter your name' />
+                                <input type="text" name="Enter your name" className='font-inter font-normal text-md leading-130 text-primary border-none outline-none w-full' placeholder='Enter your name' onChange={(e) => setFormValue({
+                                    ...formValue,
+                                    name: e.target.value
+                                })} />
                             </div>
+                            {error && formValue.name === '' && "Name is required"}
 
                             <div className='px-8 py-6 md:py-7 border-solid border-[3px] border-primary rounded-[72px] mt-5 md:mt-9 bg-white'>
-                                <input type="email" name="Enter your email" className='font-inter font-normal text-md leading-130 text-primary border-none outline-none w-full' placeholder='Enter your email' />
+                                <input type="email" name="Enter your email" className='font-inter font-normal text-md leading-130 text-primary border-none outline-none w-full' placeholder='Enter your email' onChange={(e) => setFormValue({
+                                    ...formValue,
+                                    email: e.target.value
+                                })} />
                             </div>
+                            {error && "Email is required"}
 
-                            <button className='w-full h-[78px] md:h-[90px] rounded-[72px] mt-5 md:mt-9 border-none outline-none font-inter text-xl leading-130 font-semibold text-white bg-secondry'>Submit</button>
+                            <button type='submit' onClick={(e) => handleSubmit(e)} className='w-full h-[78px] md:h-[90px] rounded-[72px] mt-5 md:mt-9 border-none outline-none font-inter text-xl leading-130 font-semibold text-white bg-secondry'>Submit</button>
                         </form>
                     </div>
                     <div className="w-full lg:w-7/12 flex justify-center lg:justify-end">
