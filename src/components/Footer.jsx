791ab7b1642d22254fc504778c53fc1e69a58a17
AmanPunia-510 <puniaaman888@gmail.com>
2024-07-25 09:38:19 +0530

    all hover effects taken seccessfully

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,15 +13,37 @@ const Footer = () => {
         <>
             <section className='mt-20 sm:mt-24 md:mt-[126px] lg:mt-[150px] xl:mt-[227px] bg-primary max-w-[1531px] mx-auto'>
                 <div className="container pt-12 xxl:max-w-[1440px]">
-                    <img src={logo} className='mx-auto max-w-[155px]' alt="logo" />
+                    <a href="#">
+                        <img src={logo} className='mx-auto max-w-[155px]' alt="logo" />
+                    </a>
                     <div className='max-w-[493px] mx-auto flex items-center gap-4 sm:gap-9 justify-center pt-11 flex-wrap'>
-                        <img src={insta} className='w-[38px] h-[38px]' alt="instagram" />
-                        <img src={face} className='w-[38px] h-[38px]' alt="face" />
-                        <img src={pinter} className='w-[38px] h-[38px]' alt="pinter" />
-                        <img src={youtube} className='w-[38px] h-[38px]' alt="youtube" />
-                        <img src={whatshapp} className='w-[38px] h-[38px]' alt="whatshapp" />
-                        <img src={inbox} className='w-[38px] h-[38px]' alt="inbox" />
-                        <img src={call} className='w-[38px] h-[38px]' alt="call" />
+                        <a href="https://www.instagram.com/">
+                            <img src={insta} className='w-[38px] h-[38px] hover:shadow-secondry hover:shadow-link rounded-[50%] transition-all' alt="instagram" />
+                        </a>
+
+                        <a href="https://www.facebook.com/">
+                            <img src={face} className='w-[38px] h-[38px] hover:shadow-secondry hover:shadow-link rounded-[50%] transition-all' alt="face" />
+                        </a>
+
+                        <a href="https://www.pinterest.com/">
+                            <img src={pinter} className='w-[38px] h-[38px] hover:shadow-secondry hover:shadow-link rounded-[50%] transition-all' alt="pinter" />
+                        </a>
+
+                        <a href="https://www.youtube.com/">
+                            <img src={youtube} className='w-[38px] h-[38px] hover:shadow-secondry hover:shadow-link rounded-[50%] transition-all' alt="youtube" />
+                        </a>
+
+                        <a href="https://www.whatsapp.com/">
+                            <img src={whatshapp} className='w-[38px] h-[38px] hover:shadow-secondry hover:shadow-link rounded-[50%] transition-all' alt="whatshapp" />
+                        </a>
+
+                        <a href="mailto:someone@example.com">
+                            <img src={inbox} className='w-[38px] h-[38px] hover:shadow-secondry hover:shadow-link rounded-[50%] transition-all' alt="inbox" />
+                        </a>
+
+                        <a href="tel:+18475555555">
+                            <img src={call} className='w-[38px] h-[38px] hover:shadow-secondry hover:shadow-link rounded-[50%] transition-all' alt="call" />
+                        </a>
                     </div>
                 </div>
                 <div className='bg-[#3D6273] w-full h-[1px] mt-[60px]'></div>
